3363391ada960db507205cb9152b957d1874e162
Daniel <dannymorison17@Gmail.com>
2015-10-26 11:16:46 +0000

    Tidied up the form to print out details properly

--- formResponse.php
+++ formResponse.php
@@ -8,3 +8,4 @@
 $forename = $_POST["forename"];
 $surname = $_POST["surname"];
 echo $forename . $surname;
+
--- helloSector.php
+++ helloSector.php
@@ -18,8 +18,8 @@
 // Creates a form and stores information, directing it to formResponse.php via the post method.
 
 <form action="formResponse.php" method="post">
-    <label> <input type=text name=“forename”/> </label>
-   <label> Surname: <input type=text name=“surname”/> </label>
+    <label> Forename: <input type="text" name=“forename”/> </label>
+   <label> Surname: <input type="text" name=“surname”/> </label>
     <input type="submit" value="Submit">
 </form>
 
