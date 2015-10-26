aa9cce5bef037fbd866e9d4b2068d4d2fe88e553
Daniel <dannymorison17@Gmail.com>
2015-10-26 10:36:43 +0000

    Initial commit

---
+++ helloPrinter.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 26/10/15
+ * Time: 10:26
+ */
+$query = $_GET["query"];
+echo "Hello " . $query;
---
+++ helloSector.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p>
+   <a href="helloPrinter.php?query=earth"> Hello Earth </a>
+</p>
+<p>
+    <a href="helloPrinter.php?query=mars"> Hello Mars </a>
+</p>
+<p>
+    <a href="helloPrinter.php?query=uranus"> Hello Uranus</a>
+</p>
+</body>
+</html>
